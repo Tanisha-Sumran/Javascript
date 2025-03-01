1713bcb461873e4db086a656dd4e6a8ce3ea22cf
Tanisha-Sumran <tanishasumran0@gmail.com>
2025-03-01 13:49:01 +0530

    Add files via upload

---
+++ guessnumgame.js
@@ -0,0 +1,6 @@
+let gamenum=20;
+let usernum=prompt("enter any number:");
+while(usernum!=gamenum){
+    usernum=prompt("u entered wrong num, guess again");
+}
+console.log("congratulations u have entered the correct number");
